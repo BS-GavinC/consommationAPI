e54ea10f1b034a116a14502f1214ad26724d114f
VZGLY <chaineux.gavin@gmail.com>
2022-12-01 13:57:21 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http : HttpClient) {
+
+    http.post<User>("http://localhost:1337/post", {id : 1, name : "paul"}).subscribe({
+      next : (data) => {
+        this.user = data
+      }
+    })
+
+  }
+
+
+  user! : User
+
   title = 'consommationAPI';
+}
+
+export interface User {
+  id : string
+  name : string
 }
